2de164ba9ad773be309bbb4c4e744059fc8e5b68
rvelse <raymond.vanvelse@netprogroup.com>
2024-10-20 22:51:04 +0200

    feat: prima

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,10 @@
+import { PrismaClient } from '@prisma/client';
 import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
-import { Form, redirect } from "@remix-run/react";
+import { json, useFetcher } from "@remix-run/react";
+import { format } from "date-fns";
+import { useEffect, useRef } from "react";
+
+export const db = new PrismaClient()
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,49 +15,78 @@ export const meta: MetaFunction = () => {
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
-  const json = Object.fromEntries(formData);
-  console.log(json);
-  return redirect("/");
+  const { date, title, content } = Object.fromEntries(formData);
+
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  if (!date || !title || !content) {
+    return json({ error: "Missing required fields" }, { status: 400 });
+  }
+
+  await db.entry.create({
+    data: {
+      date: new Date(date as string),
+      type: title as string,
+      text: content as string,
+    },
+  });
+  return json({ success: true });
 }
 
 export default function Index() {
+  const fetcher = useFetcher();
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (fetcher.state === "idle" && textAreaRef.current) {
+      textAreaRef.current.value = ""
+      textAreaRef.current?.focus();
+    }
+  }, [fetcher.state]);
+  
+
   return (
     <div className="h-screen p-12">
       <h1 className="text-4xl font-bold">Work Journal</h1>
       <p className="text-sm text-gray-400">Learning and doing</p>
 
       <div className="my-8 border rounded-md space-y-4 p-8 border-gray-600">
-        <Form method="post">
-          <p className="text-sm text-gray-400"> Create a new entry </p>
-          <div className="flex items-center gap-4 my-4">
-            <input className="bg-gray-900 p-2 rounded-md" type="date" name="date" id="date" />
-          </div>
-          <div className="flex items-center gap-4">
-            <label htmlFor="work">
-              <input id="work" className="mr-2" type="radio" name="title" placeholder="Title" />
-              Work
-            </label>
-            <label htmlFor="learning">
-              <input id="learning" className="mr-2" type="radio" name="title" placeholder="Title" />
-              Learning
-            </label>
-            <label htmlFor="other">
-              <input id="other" className="mr-2" type="radio" name="title" placeholder="Title" />
-              Other
-            </label>
-          </div>
-          <div className="mt-4">
-            <textarea
-              name="content"
-              className="w-full h-24 bg-gray-900 p-2 rounded-md"
-              rows={10}
-              placeholder="Write your entry here..."
-            />
-          </div>
-          <button className="mt-4 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" type="submit">
-            Submit
-          </button>
-        </Form>
+        <p className="text-sm text-gray-400">Create a new entry</p>
+
+        <fetcher.Form method="post">
+          <fieldset className="disabled:opacity-50" disabled={fetcher.state === "submitting"}>
+            <div className="flex items-center gap-4 my-4">
+              <input className="bg-gray-900 p-2 rounded-md" type="date" name="date" id="date" required defaultValue={format(new Date(), "yyyy-MM-dd")} />
+            </div>
+            <div className="flex items-center gap-4">
+              <label htmlFor="work">  
+                <input id="work" className="mr-2" type="radio" name="title" placeholder="Title" defaultChecked required />
+                Work
+              </label>
+              <label htmlFor="learning">
+                <input id="learning" className="mr-2" type="radio" name="title" placeholder="Title" />
+                Learning
+              </label>
+              <label htmlFor="other">
+                <input id="other" className="mr-2" type="radio" name="title" placeholder="Title" />
+                Other
+              </label>
+            </div>
+            <div className="mt-4">
+              <textarea
+                name="content"
+                className="w-full h-24 bg-gray-900 p-2 rounded-md"
+                rows={10}
+                placeholder="Write your entry here..."
+                required
+                ref={textAreaRef}
+              />
+            </div>
+            <button className="mt-4 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" type="submit">
+              { fetcher.state === "submitting" ? "Submitting..." : "Submit" }
+            </button>
+          </fieldset>
+        </fetcher.Form>
       </div>
 
 
